be485f229028874e8a5a8fad1a84bc2bcd99e99d
degraffa <ard96@cornell.edu>
2020-11-15 20:40:59 -0500

    ok this works now lmao

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/pf2e-logo.png">
+    <CharacterSelect />
+  </div>
+</template>
+
+<script>
+import CharacterSelect from './components/CharacterSelect.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CharacterSelect
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CharacterSelect.vue
@@ -0,0 +1,15 @@
+<template>
+<section>
+    <div class="buttons">
+        <b-button type="is-dark" expanded> New Character </b-button>
+        <b-button type="is-dark" expanded> Load Character </b-button>
+    </div>
+</section>
+</template>
+
+<style>
+.buttons {
+    width: 800px;
+    margin: 0 auto;
+}
+</style>
